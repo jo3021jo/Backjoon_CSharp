8c268bc7b41c6bc276d9315b36409b5e6da63b84
jo3021jo <jo3021jo@naver.com>
2026-01-01 09:58:15 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 44 ms, Memory: 5476 KB -BaekjoonHub

---
+++ C#/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program{
+    static void Main(string[] args){
+        int a = 2541 - 1998;
+        string input = Console.ReadLine();
+        int b = int.Parse(input);
+        Console.WriteLine(b - a);
+        
+    }
+}

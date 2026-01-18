a5ee1260449028da70eeb53792194132baeaba78
jo3021jo <jo3021jo@naver.com>
2026-01-18 10:22:54 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int A, B;
+
+	while (true) {
+		cin >> A >> B;
+
+		if (A == 0 && B == 0) {
+			break;
+		}
+
+		cout << A + B << "\n";
+	}
+}

96ad1c4dcf41d90ec175054d08d6796a703e676e
jo3021jo <jo3021jo@naver.com>
2026-01-30 10:53:07 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2562. 최댓값/최댓값.cc
+++ C++17/백준/Bronze/2562. 최댓값/최댓값.cc
@@ -5,27 +5,20 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int A;
 	int arr[9];
-	int count = 0;
-	int max = 0;
 
 	for (int i = 0; i < 9; i++) {
-		cin >> A;
-		arr[i] = A;
-	}
-	
-	for (int i = 0; i < 9; i++) {
-		if (max < arr[i]) {
-			max = arr[i];
-		}
+		cin >> arr[i];
 	}
 
+	int mx = arr[0];
+	int index = 1;
+
 	for (int i = 0; i < 9; i++) {
-		if (max == arr[i]) {
-			count = i+1;
+		if (arr[i] > mx) {
+			mx = arr[i];
+			index = i + 1;
 		}
 	}
-	
-	cout << max << "\n" << count << "\n";
+	cout << mx << "\n" << index << "\n";
 }
